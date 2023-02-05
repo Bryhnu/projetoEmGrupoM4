3211b2d358f91332006ce8736206d81460f46f29
MaiaraRib388 <113558275+MaiaraRib388@users.noreply.github.com>
2023-02-04 21:12:00 -0300

    Criei o arquivo para as perguntas
    Fiz o código pra uma pergunta da página "liga"
    E fiz 3 perguntas para a página "times"
    Juntamente com os códigos

---
+++ Dados sql/perguntas.sql
@@ -0,0 +1,30 @@
+-- Quem são os 5 líderes da conferências leste e oeste ?
+
+
+
+
+-- Os 5 times com as maiores sequências de derrota da temporada
+
+
+
+-- Os 5 times com as maiores sequências de derrota da temporada
+SELECT * FROM `tabela` 
+ORDER BY `classificacao` ASC 
+LIMIT 5;
+
+-- Quais os 5 times mais antigos?
+SELECT * FROM `times` 
+ORDER BY `YEARFOUNDED` ASC 
+LIMIT 5;
+
+
+--Quais os 5 times mais novos?
+SELECT * FROM `times` 
+ORDER BY `YEARFOUNDED` DESC 
+LIMIT 5;
+
+
+--Quais os 5 times que têm mais capacidade de arena?
+SELECT * FROM `times` 
+ORDER BY `ARENACAPACITY` DESC 
+LIMIT 5;
